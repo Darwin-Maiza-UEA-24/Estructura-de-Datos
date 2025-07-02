cb0da2088138ad2863cc113de264146b7191eb8f
Darwin_Maiza_UEA_2024 <dd.maizaz@uea.edu.ec>
2025-07-01 20:07:49 -0500

    Titulo y tema

---
+++ Semana_5/Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Escribir un programa que almacene las asignaturas de un curso 
+//(por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+
+Console.WriteLine("Universidad Estatal Amazónica");
+Console.WriteLine(-------------------------------) 
