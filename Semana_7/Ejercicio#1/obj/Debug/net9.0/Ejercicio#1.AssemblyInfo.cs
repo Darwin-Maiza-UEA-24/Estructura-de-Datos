ab07030338dd438f2130279ec746c2d9ee130040
Darwin-Maiza-UEA-24 <dd.maizaz@uea.edu.ec>
2025-07-15 03:52:44 -0500

    terminado

--- Semana_7/Ejercicio#1/obj/Debug/net9.0/Ejercicio#1.AssemblyInfo.cs
+++ Semana_7/Ejercicio#1/obj/Debug/net9.0/Ejercicio#1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio#1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fdeab85e20bfb5b84abf34a3194e205a93a697c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4caf623548ccc49505b1e8b27cf9d04fb9afbbc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio#1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio#1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
