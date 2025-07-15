45bafcdd68b5ab1d7b04ac854c6732c2ea0e617a
Darwin-Maiza-UEA-24 <dd.maizaz@uea.edu.ec>
2025-07-15 02:21:48 -0500

    problemas de run

--- Semana_7/Ejercicio_01/Program.cs
+++ Semana_7/Ejercicio_01/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hellobbbbbbbbbbbbbbbbbbbbbbbbb, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class ParentesisBalanceados
+{
+    static bool EstaBalanceada(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+
+        foreach (char c in expresion)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                pila.Push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                if (pila.Count == 0)
+                    return false;
+
+                char ultimo = pila.Pop();
+                if ((c == ')' && ultimo != '(') ||
+                    (c == '}' && ultimo != '{') ||
+                    (c == ']' && ultimo != '['))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return pila.Count == 0;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Ingrese una expresión matemática:");
+        string expresion = Console.ReadLine();
+
+        if (EstaBalanceada(expresion))
+            Console.WriteLine("Fórmula balanceada.");
+        else
+            Console.WriteLine("Fórmula no balanceada.");
+    }
+}
